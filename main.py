799a553da5f119e1cab056e521217deafa23ac02
unknown <minz.manohar@gmail.com>
2022-11-12 21:14:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print("Welcome to Band name generator")
+    city = input("Whats the name of the city you grew up in?\n")
+    pet = input("Whats your pet name?\n")
+    print(f"Your pet name could be {city} {pet}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
